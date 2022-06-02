3c5afa9b0d76191c9625819aa34d9ab755258e26
MaximoRomano <102158982+MaximoRomano@users.noreply.github.com>
2022-06-02 08:26:02 -0300

    Initial commit

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float movementspeed;
+    public float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, movementspeed);
+            transform.Translate(0,0,movementspeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += new Vector3(0, 0, movementspeed);
+            transform.Translate(0, 0, -movementspeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position += new Vector3(0, 0, movementspeed);
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position += new Vector3(0, 0, movementspeed);
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+
+
+    }
+     
+}
